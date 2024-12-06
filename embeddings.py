d8656a00b3e20827d57839100a91c21aa515f2a2
Aidan Colon <77650375+aidcol@users.noreply.github.com>
2024-12-05 20:54:17 -0600

    Implement functions to load computed embeddings

--- embeddings.py
+++ embeddings.py
@@ -1,10 +1,14 @@
 import os
 from pathlib import Path
+import pickle
 import re
 import csv
+import argparse
 
 import requests
 import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 import librosa
 from transformers import Wav2Vec2FeatureExtractor
 from transformers import AutoModel
@@ -22,6 +26,20 @@ USER_AGENT = "RiceUDataBot/0.1.0"
 
 # Maximum duration of the audio clips in seconds
 MAX_DURATION_S = 30
+
+# Path to the directory containing the MERT embeddings as .npy files
+EMBEDDINGS_DIR = os.getcwd() + "\\data\\mert_embeds"
+
+# Path to the CSV file containing the mapping from numpy file name to 
+# the original WikiMuTe file name
+IDX_TO_FILENAME_CSV = EMBEDDINGS_DIR + "\\progress.csv"
+
+# Path to the CSV file containing the exact overlaps between the WikiMuTe 
+# dataset and the Spotify Playlists dataset
+OVERLAPS_CSV = os.getcwd() + "\\overlaps\\exact_overlaps.csv"
+
+# Path to the directory to save network data
+MERTNET_DIR = os.getcwd() + "\\data\\mertnet"
 
 
 def sanitize_filename(filename):
@@ -92,8 +110,8 @@ def compute_mert_embedding(model, processor, audio_data, sr):
     return time_reduced_hidden_states.cpu().numpy()
 
 
-def process_wikimute_data(model, df, temp_dir, output_dir, resample_rate,
-                          progress_file, debug=False):
+def process_wikimute_data(model, processor, df, temp_dir, output_dir, 
+                          resample_rate, progress_file, debug=False):
     """Compute MERT embeddings for audio clips in the WikiMuTe dataset.
 
     MERT embeddings are saved as .npy files in a specified output directory.
@@ -187,7 +205,90 @@ def process_wikimute_data(model, df, temp_dir, output_dir, resample_rate,
     os.rmdir(temp_path)
 
 
-if __name__ == '__main__':    
+def get_overlap_set(overlaps_csv):
+    """Get the set of overlapping tracks between the WikiMuTe dataset and the 
+    Spotify Playlists dataset.
+    
+    Args:
+        overlaps_csv (str): The path to the CSV file containing the exact overlaps.
+    
+    Returns:
+        set: A set of tuples containing the artist and song name of the overlapping tracks.
+    """
+    overlap = set()
+    with open(overlaps_csv, mode='r', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            overlap.add(row['Original Name'])
+    return overlap
+
+
+def get_idx_to_filename(idx_to_filename_csv):
+    """Load the index to file mapping from the CSV file."""
+    if os.path.exists(idx_to_filename_csv):
+        with open(idx_to_filename_csv, 'r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            idx_to_file = {int(rows[0]): rows[1] for rows in reader}
+        return idx_to_file
+    return {}
+
+
+def load_embeddings(dir, filtered=True):
+    """Load all .npy files in the specified directory into a single matrix.
+    
+    Args:
+        directory (str): The path to the directory containing the .npy files.
+    
+    Returns:
+        np.ndarray: A single matrix containing all the embeddings.
+    """
+    overlap = get_overlap_set(OVERLAPS_CSV) if filtered else set()
+    idx_to_file = get_idx_to_filename(IDX_TO_FILENAME_CSV)
+
+    embeddings = []
+    row_to_file = {}
+
+    for idx, npy_file in enumerate(tqdm(os.listdir(dir),
+                                    total=len(os.listdir(dir)),
+                                    desc='Loading embeddings')):
+        
+        if not npy_file.endswith(".npy"):
+            continue
+
+        npy_file_idx = os.path.splitext(npy_file)[0]
+        orig_file = idx_to_file[int(npy_file_idx)]
+
+        if not filtered or orig_file in overlap:
+            embedding = np.load(os.path.join(dir, npy_file))
+            embedding = embedding.flatten()
+            embeddings.append(embedding)
+            row_to_file[idx] = orig_file
+
+    X = np.vstack(embeddings)
+    return X, row_to_file
+
+
+def pca_reduce(matrix, n_components=.99):
+    """Reduce the dimensionality of the embeddings using PCA and reduce the correlation between the vectors.
+    
+    Args:
+        matrix (np.ndarray): The embeddings matrix.
+        n_components (int or None): Number of components to keep. If None, all components are kept.
+        plot_variance (bool): Whether to plot the explained variance ratio.
+    
+    Returns:
+        np.ndarray: The transformed embeddings matrix.
+    """
+    scaler = StandardScaler()
+    scaled_matrix = scaler.fit_transform(matrix)
+    
+    pca = PCA(n_components=n_components)
+    pca_matrix = pca.fit_transform(scaled_matrix)
+    
+    return pca_matrix
+
+
+def preprocess():
     cwd = os.getcwd()
     PATH_TO_WIKIMUTE_DATA = cwd + '\\wikimute\\data\\all.csv'
 
@@ -211,7 +312,41 @@ if __name__ == '__main__':
     print('Processing the WikiMuTe data...')
     print('The resampling rate is:', resample_rate)
 
-    process_wikimute_data(model, dataset, temp_dir, output_dir, resample_rate, 
-                          progress_file)
+    process_wikimute_data(model, processor, dataset, temp_dir, output_dir, 
+                          resample_rate, progress_file)
 
     print('Done processing WikiMuTe data.')
+
+
+def load():
+    mertnet_path = Path(MERTNET_DIR)
+    mertnet_path.mkdir(parents=True, exist_ok=True)
+
+    print("Loading MERT embeddings...")
+    embeddings, filenames = load_embeddings(EMBEDDINGS_DIR)
+    
+    # The X matrix is the centered data matrix where each row is a component
+    # of the MERT embedding and each column is a node in the inferred graph
+    np.save(mertnet_path / 'embeddings.npy', embeddings)
+
+    # Reduce the dimensionality of the embeddings using PCA
+    print("Reducing dimensionality of embeddings by PCA...")
+    embeddings_pca = pca_reduce(embeddings, n_components=.99)
+    np.save(mertnet_path / 'embeddings_pca.npy', embeddings_pca)
+
+    # Save the original filenames in the order of the rows of the data matrix
+    with open(mertnet_path / 'embeddings_filenames.pkl', 'wb') as f:
+        pickle.dump(filenames, f)
+
+
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser(description='Process or load embeddings.')
+    parser.add_argument('--mode', choices=['preprocess', 'load'], required=True,
+                        help='Specify whether to run the preprocess or load_embeddings function.')
+    
+    args = parser.parse_args()
+    
+    if args.mode == 'preprocess':
+        preprocess()
+    elif args.mode == 'load':
+        load()
